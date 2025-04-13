99d96ab05280204a02b36612aeeaf2910209bc88
Ahmed Ayman Alhofy <ahmedalhofy42@gmail.com>
2025-04-14 00:16:50 +0200

    fix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -21,3 +22,4 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+module.exports = nextConfig;
